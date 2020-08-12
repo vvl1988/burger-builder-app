f07b2f6a4c035f9f0e14e12d0ae577888293da88
lvv1988 <lvv1988@gmail.com>
2020-08-12 19:03:27 +0700

    - Render Burger with Ingredients sent from BurgerBuilder Container

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,15 +2,26 @@ import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
-const burger = (props) => (
-  <div className={classes.Burger}>
-    <BurgerIngredient type="bread-top" />
-    <BurgerIngredient type="salad" />
-    <BurgerIngredient type="meat" />
-    <BurgerIngredient type="cheese" />
-    <BurgerIngredient type="salad" />
-    <BurgerIngredient type="bread-bottom" />
-  </div>
-);
+const burger = (props) => {
+  let transformIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, index) => (
+      <BurgerIngredient key={igKey + index} type={igKey} />
+    ));
+  })
+  .reduce((arr, el) => {
+      return arr.concat(el)
+  }, []);
+
+  if(transformIngredients.length===0) {
+    transformIngredients = <p>Please adding some ingredients!</p>
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
 
 export default burger;
