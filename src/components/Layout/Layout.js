68de86bea49b9a85f2f023dc5df5d73cebf3308b
lvv1988 <lvv1988@gmail.com>
2020-08-15 14:40:00 +0700

    - Add Toolbar with Burger Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
   <React.Fragment>
     <div> Toolbars, SideDrawer, Backdrop</div>
+    <Toolbar/>
     <main className={classes.Content}>{props.children}</main>
   </React.Fragment>
 );
