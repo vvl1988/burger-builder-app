9f0c67c65ca2a5b48c851fa7fea867ee8cd576ae
lvv1988 <lvv1988@gmail.com>
2020-08-17 08:54:21 +0700

    - improve performance a bit by not updating OrderSummary when not necesarry

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => (
-    <li key={igKey}>
-      <span style={{ textTransform: "capitalize" }}>{igKey}</span> :{" "}
-      {props.ingredients[igKey]}
-    </li>
-  ));
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        Total Price: <strong>{props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button buttonType="Danger" clicked={props.purchaseCanceled}>
-        CANCEL
-      </Button>
-      <Button buttonType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </React.Fragment>
-  );
-};
+class OrderSummary extends Component {
+  // Should be a functional component, just make it to be class-base component for debug purpose
+  componentWillUpdate(){
+    console.log('[OrderSummary] WillUpdate');
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span> :{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      )
+    );
 
-export default orderSummary;
+    return (
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          Total Price: <strong>{this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button buttonType="Danger" clicked={this.props.purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button buttonType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default OrderSummary;
