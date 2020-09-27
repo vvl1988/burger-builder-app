007f714abaa4c80bef3150fd64b0d6e3c3e051b1
lvv1988 <lvv1988@gmail.com>
2020-09-28 06:34:29 +0700

    - guard routes when not authenticated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import {connect} from "react-redux";
-import * as actions from "./store/actions/index"
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
@@ -10,29 +10,46 @@ import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 
 class App extends Component {
-  componentWillMount(){
+  componentWillMount() {
     this.props.onAutoLogin();
   }
 
   render() {
-    return(
-    <div className="App">
-      <Layout>
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/"/>
+      </Switch>
+    );
+    if (this.props.isAuthenticated) {
+      routes = (
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/"/>
         </Switch>
-      </Layout>
-    </div>
+      );
+    }
+    return (
+      <div className="App">
+        <Layout>
+          {routes}
+        </Layout>
+      </div>
     );
-  } 
+  }
 }
 
-const mapDispatchToProps = dispatch => {
-  return{onAutoLogin: ()=>dispatch(actions.checkAuthState())}
-}
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return { onAutoLogin: () => dispatch(actions.checkAuthState()) };
+};
 
-export default connect(null, mapDispatchToProps)(App);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
