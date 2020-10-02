59229cc9e35c27741c0fdf0fb19dfce89ac50f7b
lvv1988 <lvv1988@gmail.com>
2020-10-02 19:57:04 +0700

    - deploying to firebase - fix issue when running on browser without redux devtool on Dev mode

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import authReducer from "./store/reducers/auth";
 
 const rootReducer = combineReducers({burger: burgerBuilderReducer, order:orderReducer, auth: authReducer});
 
-const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = (process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk)
   ));
